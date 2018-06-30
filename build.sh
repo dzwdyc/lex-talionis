f6515c8ae7ba3b2804ca7d24d4148db7cfe48990
rainlash <rainlash>
2018-06-29 21:24:21 -0500

    Updating build process

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 # Build Script for the_lion_throne
 # In Data/config.ini, turn cheat to 0
 # In Data/config.ini, turn Screen Size to 3 and temp_ScreenSize to 3
-# Make sure your old copy of the lion throne is backed up somewhere
 pyinstaller main.spec
-rm -rf ../the_lion_throne
-mv dist/the_lion_throne ../the_lion_throne
-cp ../lex-talionis-utilities/audio_dlls/* ../the_lion_throne/
+rm -rf ../cosmic_demo
+mv dist/cosmic_demo ../cosmic_demo
+cp ../lex-talionis-utilities/audio_dlls/* ../cosmic_demo/
 echo Done
